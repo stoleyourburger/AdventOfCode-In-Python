21dd38fd51c06219ddc20d9a933d9f2dac1b8c18
StoleYourBurger <stoleyourburger@gmail.com>
2024-02-03 23:56:37 +0100

    Day 2 Part Two

--- 2015/Day1.py
+++ 2015/Day1.py
@@ -1,7 +1,7 @@
 # --- PART ONE ---
 
 # Putting the content of the data file in the variable
-with open('Day1_input.txt', 'r') as f:
+with open("Day1_input.txt", "r") as f:
     input = f.read()
 
 # Variable to count current floor
@@ -9,13 +9,13 @@ currentFloor = 0
 
 # Simple iteration over each char and incrementing or decrementing the counter
 for char in input:
-    if char == '(':
+    if char == "(":
         currentFloor += 1
-    elif char == ')':
+    elif char == ")":
         currentFloor -= 1
 
 # Showing the result in console
-print(f'Santa gets to {currentFloor} floor')
+print(f"Santa gets to {currentFloor} floor")
 
 # --- PART TWO ---
 
@@ -26,12 +26,12 @@ position = 0
 # Based on the '(' or ')' we increment or decrement the counter. We always increment the position variable
 # When the counter variable reaches -1, we check the position variable, write it in the console and break the loop
 for char in input:
-    if char == '(':
+    if char == "(":
         counter += 1
         position += 1
-    elif char == ')':
+    elif char == ")":
         counter -= 1
         position += 1
     if counter == -1:
-        print(f'Santa enters the basement at {position} position')
+        print(f"Santa enters the basement at {position} position")
         break
--- 2015/Day2.py
+++ 2015/Day2.py
@@ -3,18 +3,18 @@
 totalWrap = 0
 
 # Putting the content of the data file in the variable
-with open('Day2_input.txt', 'r') as f:
+with open("Day2_input.txt", "r") as f:
     for line in f:
 
-        # Declaring the array
-        array = []
-        # Splitting the dimensions of the box in each line and removing the whitespace with rstrip() 
-        array += line.rstrip().split('x')
+        # Declaring the list
+        list = []
+        # Splitting the dimensions of the box in each line and removing the whitespace with rstrip()
+        list += line.rstrip().split("x")
 
-        # Getting length, width and height from the array and assigning to the variables
-        length = int(array[0])
-        width = int(array[1])
-        height = int(array[2])
+        # Getting length, width and height from the list and assigning to the variables
+        length = int(list[0])
+        width = int(list[1])
+        height = int(list[2])
 
         # Calculating one of each sides of the box (Don't forget that there are two of each sides!)
         sideA = length * width
@@ -29,5 +29,41 @@ with open('Day2_input.txt', 'r') as f:
 
         # Adding this box area to total Paper
         totalWrap += surfaceArea
-    
-print(f'You need {totalWrap} sq feet of wrapping paper')
+
+print(f"You need {totalWrap} sq feet of wrapping paper")
+
+# --- PART TWO ---
+
+totalRibbon = 0
+
+# Again we need to iterate over each of the boxes
+with open("Day2_input.txt", "r") as f:
+    for line in f:
+
+        # Declaring the list and splitting the dimensions of the box in each line
+        list = []
+        list += line.rstrip().split("x")
+
+        # Converting all strings in list to integers
+        list = [eval(i) for i in list]
+
+        # Sorting the list for values to be from smallest to largest
+        list = sorted(list)
+
+        # Getting length, width and height from the list and assigning to the variables
+        length = int(list[0])
+        width = int(list[1])
+        height = int(list[2])
+
+        # Calculating the ribbon for each present
+        singleRibbon = (
+            int(list[0])
+            + int(list[0])
+            + int(list[1])
+            + int(list[1])
+            + length * width * height
+        )
+
+        totalRibbon += singleRibbon
+
+print(f"You need {totalRibbon} feet of ribbon")
