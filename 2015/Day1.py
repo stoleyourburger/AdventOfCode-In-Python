4e620505d7e3ae57d6e67d402a147a44ad409256
StoleYourBurger <stoleyourburger@gmail.com>
2024-02-03 22:45:42 +0100

    Day 1 Part Two

--- 2015/Day1.py
+++ 2015/Day1.py
@@ -1,4 +1,4 @@
-# --- DAY ONE ---
+# --- PART ONE ---
 
 # Putting the content of the data file in the variable
 with open('Day1_input.txt', 'r') as f:
@@ -17,3 +17,21 @@ for char in input:
 # Showing the result in console
 print(f'Santa gets to {currentFloor} floor')
 
+# --- PART TWO ---
+
+# Variables, one is to count ups & downs, another is to count a position
+counter = 0
+position = 0
+
+# Based on the '(' or ')' we increment or decrement the counter. We always increment the position variable
+# When the counter variable reaches -1, we check the position variable, write it in the console and break the loop
+for char in input:
+    if char == '(':
+        counter += 1
+        position += 1
+    elif char == ')':
+        counter -= 1
+        position += 1
+    if counter == -1:
+        print(f'Santa enters the basement at {position} position')
+        break
