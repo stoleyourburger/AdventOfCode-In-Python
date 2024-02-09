8b93b5a4683984d910d177864fcf75c2fbdf165c
StoleYourBurger <stoleyourburger@gmail.com>
2024-02-09 22:47:50 +0100

    Day 12 Part Two

--- 2015/Day12.py
+++ 2015/Day12.py
@@ -20,3 +20,37 @@ for number in result:
     total += int(number)
 
 print(f"Final number is: {total}")
+
+# --- PART TWO ---
+
+# Another approach with actually working with keys/values
+input = json.loads(open("Day12_input.txt", "r").read())
+
+
+# Wrapping everything in a function
+def TotalNumbers(input):
+
+    # Comparing the input type to the dictionary type
+    if type(input) == type(dict()):
+
+        # Excluding every occurence of 'red' with everything past it
+        # Not really excluding but returning 0 instead of whatever is inside
+        if "red" in input.values():
+            return 0
+
+        # If 'red' is not inside, we map (execute) the function with input.values() to check for other types in values
+        return sum(map(TotalNumbers, input.values()))
+
+    if type(input) == type(list()):
+
+        # For each list recursively sum all the values
+        return sum(map(TotalNumbers, input))
+
+    # If nothing is left we return the input
+    if type(input) == type(0):
+        return input
+
+    return 0
+
+
+print(f"Second number is: {TotalNumbers(input)}")
