68a89a69bee33595d4256086d8de6cfb71e0c1f8
StoleYourBurger <stoleyourburger@gmail.com>
2024-02-13 15:58:14 +0100

    Day 13 Part Two

--- 2015/Day13.py
+++ 2015/Day13.py
@@ -40,3 +40,51 @@ for perm in perms:
 
 # And printing the biggest result possible
 print(result)
+
+# --- PART TWO ---
+
+# We just add ourselves to the names list and happiness dictionary with 0 value for happiness
+# Everything else is just the same
+
+names = []
+
+for line in input:
+    line = line.split()
+    if line[0] not in names:
+        names.append(line[0])
+
+# Here
+names.append("Me")
+
+happiness = {}
+
+# We find all possible combinations of names and their happiness value with already minus sign added if needed based on 'lose' in the line
+for line in input:
+    words = line[:-2].split(" ")
+    happiness_value = int(words[3])
+    if words[2] == "lose":
+        happiness_value *= -1
+    happiness[(words[0], words[-1])] = happiness_value
+
+    # And here
+    happiness[("Me", words[0])] = 0
+    happiness[(words[0], "Me")] = 0
+
+perms = itertools.permutations(names)
+
+result = 0
+
+# We iterate on all the possible combinations of names and for every combination adding (or subtracting with the '-' sign there) happiness
+# We have done this (or similar) before therefore I don't put much comments
+for perm in perms:
+    happy_value = 0
+    for i in range(len(perm) - 1):
+        happy_value += (
+            happiness[(perm[i], perm[i + 1])] + happiness[(perm[i + 1], perm[i])]
+        )
+    happy_value += happiness[(perm[-1], perm[0])] + happiness[(perm[0], perm[-1])]
+    if happy_value > result:
+        result = happy_value
+
+# And printing the biggest result possible
+print(result)
